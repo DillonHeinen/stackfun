3e7fb11103d70065eba5db2db69488f04c8ff933
DillonHeinen <56735131+DillonHeinen@users.noreply.github.com>
2020-02-25 01:52:25 -0600

    Update stack312_arr.c

--- stack312_arr.c
+++ stack312_arr.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "stack312_arr.h"
 
+void makeStack(Stack312 *s) {
+    s->top = -1;
+}
+
+bool isFull(Stack312 s) {
+    if(s.top >= 80) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool isEmpty(Stack312 s) {
+    if(s.top == -1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void push(StackEntry e, Stack312 *s) {
+    s->top++;
+    s->elements[s->top] = e;
+}
+
+StackEntry pop(Stack312 *s) {
+    StackEntry temp = s->elements[s->top];
+    s->top--;
+    return temp;
+}
