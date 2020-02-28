38e8d6bac2ccbc03d59df178b04b52649a2845d5
DillonHeinen <56735131+DillonHeinen@users.noreply.github.com>
2020-02-28 13:02:55 -0600

    Final Submission

--- driver_arr.c
+++ driver_arr.c
@@ -1,6 +1,34 @@
+//life_driver.c
+//
+//Life Driver program
+//Roger Priebe 9/5/2018
+//EE 312
+
+
+/* Student information for project:
+ *
+ * Replace <NAME> with your name.
+ *
+ * On my honor, <NAME>, this programming project is my own work
+ * and I have not provided this code to any other student.
+ *
+ * Name: Dillon Heinen
+ * email address: dillonheinen@utexas.edu
+ * UTEID: deh2486
+ * Section 5 digit ID: 16085
+ *
+ */
+
+//This file is a driver program for the life.c module.
+//This program must work with your implementations life.h and life.c
+//Do not change the function definitions for populateWorld, showWorld or
+//iterateGeneration.
+//
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "stack312_arr.h"
 
 int main(int argc, char *argv[]) {
@@ -8,113 +36,76 @@ int main(int argc, char *argv[]) {
     Stack312 s;
     makeStack(&s);
 
-//    char *fname = (char*) malloc(strlen(argv[1]));
-//    strcpy(fname, argv[1]);
-    char fname[] = "C:\\Users\\dillon\\Desktop\\exp.txt";
+    char *fname = (char*) malloc(strlen(argv[1]) + 1);
+    strcpy(fname, argv[1]);
+//    char fname[] = "C:\\Users\\dillon\\Desktop\\exp.txt";
 
     FILE *fptr;
     fptr = fopen(fname, "r");
 
-    if(fptr == NULL) {
+    if (fptr == NULL) {          // checks for empty file
         printf("File Error\n");
         exit(-1);
     } else {
         char buf[BUFSIZ];
 
-        while(fgets(buf, sizeof(buf), fptr)) {
-            for(int i = 0; i < strlen(buf); i++) {
-                if((buf[i] == '(') || (buf[i] == ')') ||
-                   (buf[i] == '<') || (buf[i] == '>') ||
-                   (buf[i] == '[') || (buf[i] == ']')) {
+        while (fgets(buf, sizeof(buf), fptr)) {               // read in file one line at a time
+            char bufferCheck[BUFSIZ];            // prevents false trigger of valid printout
+            strcpy(bufferCheck, buf);
+            int i = 0;
+            int endmarker = 0;
+            while((i < strlen(buf)) && (endmarker == 0)) {
+                if ((buf[i] == '(') || (buf[i] == '<') || (buf[i] == '[')) {     // checks for an opening brace
                     push(buf[i], &s);
+                } else if ((isEmpty(s)) && (((buf[i] == ')') || (buf[i] == '>') || (buf[i] == ']')))) {      // checks for incorrect closing brace
+                    if (buf[i] == ')') {
+                        buf[strcspn(buf, "\n")] = 0;
+                        printf("%s === missing (\n", buf);
+                    } else if (buf[i] == '>') {
+                        buf[strcspn(buf, "\n")] = 0;
+                        printf("%s === missing <\n", buf);
+                    } else if (buf[i] == ']') {
+                        buf[strcspn(buf, "\n")] = 0;
+                        printf("%s === missing [\n", buf);
+                    }
+                } else if (((buf[i] == ')') || (buf[i] == '>') || (buf[i] == ']'))) {      // checks for general case of incorrect brace
+                    char popHold = pop(&s);
+
+                    if ((buf[i] == ')') && (popHold != '(')) {
+                        buf[strcspn(buf, "\n")] = 0;
+                        printf("%s === missing )\n", buf);
+                        endmarker = 1;
+                    } else if ((buf[i] == '>') && (popHold != '<')) {
+                        buf[strcspn(buf, "\n")] = 0;
+                        printf("%s === missing >\n", buf);
+                        endmarker = 1;
+                    } else if ((buf[i] == ']') && (popHold != '[')) {
+                        buf[strcspn(buf, "\n")] = 0;
+                        printf("%s === missing ]\n", buf);
+                        endmarker = 1;
+                    }
                 }
+                i++;
             }
-            if(isEmpty(s) == true) {
-                printf("There are no expressions to evaluate");
-            } else {
-                int rightParCount = 0;
-                int rightAngCount = 0;
-                int rightBraCount = 0;
-                int leftParCount = 0;
-                int leftAngCount = 0;
-                int leftBraCount = 0;
+            if(isEmpty(s) == false) {       // checks for general case of missing brace
+                char holder;
+                holder = pop(&s);
+                if(holder == '(') {
+                    buf[strcspn(buf, "\n")] = 0;
+                    printf("%s === missing )\n", buf);
 
-                while(isEmpty(s) != true) {
-                    char check = pop(&s);
-                    if(check == '(') {
-                        leftParCount++;
-                    } else if(check == '<') {
-                        leftAngCount++;
-                    } else if(check == '[') {
-                        leftBraCount++;
-                    } else if(check == ')') {
-                        rightParCount++;
-                    } else if(check == '>') {
-                        rightAngCount++;
-                    } else if(check == ']') {
-                        rightBraCount++;
-                    }
+                } else if(holder == '<') {
+                    buf[strcspn(buf, "\n")] = 0;
+                    printf("%s === missing >\n", buf);
+                } else if(holder == '[') {
+                    buf[strcspn(buf, "\n")] = 0;
+                    printf("%s === missing ]\n", buf);
                 }
-
-                if((rightParCount == leftParCount) && (rightAngCount == leftAngCount) && (rightBraCount == leftBraCount)) {
-                    printf("%s === valid expression\n", buf);
-                } else {
-                    printf("%s === missing ", buf);
-
-                    if(rightParCount != leftParCount) {
-                        if(rightParCount < leftParCount) {
-                            int compare;
-                            compare = leftParCount - rightParCount;
-                            while(compare != 0) {
-                                printf(")");
-                                compare--;
-                            }
-                        } else {
-                            int compare;
-                            compare = rightAngCount - leftAngCount;
-                            while(compare != 0) {
-                                printf("(");
-                                compare--;
-                            }
-                        }
-                    }
-                    if(rightAngCount != leftAngCount) {
-                        if(rightAngCount < leftAngCount) {
-                            int compare;
-                            compare = leftAngCount - rightAngCount;
-                            while(compare != 0) {
-                                printf(">");
-                                compare--;
-                            }
-                        } else {
-                            int compare;
-                            compare = rightAngCount - leftAngCount;
-                            while(compare != 0) {
-                                printf("<");
-                                compare--;
-                            }
-                        }
-                    }
-                    if(rightBraCount != leftBraCount) {
-                        if(rightBraCount < leftBraCount) {
-                            int compare;
-                            compare = leftBraCount - rightBraCount;
-                            while(compare != 0) {
-                                printf("]");
-                                compare--;
-                            }
-                        } else {
-                            int compare;
-                            compare = rightBraCount - leftBraCount;
-                            while(compare != 0) {
-                                printf("[");
-                                compare--;
-                            }
-                        }
-                    }
-                    printf("\n");
-                }
+            } else if ((isEmpty(s) == true) && (strcmp(buf, bufferCheck) != 0)){
+                printf("%s === valid expression\n", buf);
             }
         }
-    }
+    } 
+    free(fname);
+    fclose(fptr);
 }
