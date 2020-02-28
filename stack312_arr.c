cfe97f89e125fbf124bfc2b445ae205825183810
DillonHeinen <56735131+DillonHeinen@users.noreply.github.com>
2020-02-28 13:03:33 -0600

    Final Submission

--- stack312_arr.c
+++ stack312_arr.c
@@ -2,10 +2,14 @@
 #include <stdbool.h>
 #include "stack312_arr.h"
 
+//Preconditions: Pass pointer to struct of type Stack312
+//Postconditios: Stack is initialized
 void makeStack(Stack312 *s) {
     s->top = -1;
 }
 
+//Preconditions: Pass struct of type Stack312 by value
+//Postconditios: Returns true or false
 bool isFull(Stack312 s) {
     if(s.top >= 80) {
         return true;
@@ -14,6 +18,8 @@ bool isFull(Stack312 s) {
     }
 }
 
+//Preconditions: Pass struct of type Stack312 by value
+//Postconditios: Returns true or false
 bool isEmpty(Stack312 s) {
     if(s.top == -1) {
         return true;
@@ -22,11 +28,15 @@ bool isEmpty(Stack312 s) {
     }
 }
 
+//Preconditions: Pass stack entry and pointer to a stack
+//Postconditions: Pushes entry on stack
 void push(StackEntry e, Stack312 *s) {
     s->top++;
     s->elements[s->top] = e;
 }
 
+//Preconditions: Pass stack entry and pointer to a stack
+//Postconditios: Pops entry on stack
 StackEntry pop(Stack312 *s) {
     StackEntry temp = s->elements[s->top];
     s->top--;
